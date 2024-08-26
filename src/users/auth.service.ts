bfb5c625f961e1c851b003ea5226a7b2a5ed53be
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-26 10:55:09 +0330

    008 Setting up Sessions

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,41 +1,49 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
+import { User } from './user.entity';
 
 const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
-
-  async signup(email:string, password:string){
+  async signup(email: string, password: string) {
     // see if email is in use
     const inUse = await this.usersService.find(email);
-    console.log(inUse)
-    if(inUse.length){
-      throw new BadRequestException('email in use.')
+    console.log(inUse);
+    if (inUse.length) {
+      throw new BadRequestException('email in use.');
     }
-
     // hash the user's password
     // Generate a salt
     const salt = randomBytes(8).toString('hex');
-    console.log('salt : ', salt)
-
+    console.log('salt : ', salt);
     // Hash the salt and the password togrther
     const hash = (await scrypt(password, salt, 32)) as Buffer;
-    
-
     // Join the hashed result and store it in db
-    const result = salt + '.' + hash.toString('hex')
-
+    const result = salt + '.' + hash.toString('hex');
     // create a new user and save it
     const user = await this.usersService.create(email, result);
     // return user
     return user;
   }
 
-  signin(){
-
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('No user with such email!');
+    }
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Password is not correct!');
+    }
+    return user;
   }
 }
