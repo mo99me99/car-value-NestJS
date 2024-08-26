c0900102ad632dfeebac5657f5ed514774e0a800
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-26 08:58:02 +0330

    006 Wrapping the Interceptor in a Decorator

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,8 +9,13 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
 
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dot:any){}
+  constructor(private dot: any) {}
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,13 +8,11 @@ import {
   Query,
   Delete,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
@@ -26,7 +24,7 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('Handler is running.');
