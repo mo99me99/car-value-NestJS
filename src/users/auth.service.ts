df72186a3f6a1415dc636bfc5ebb8a11a15ac1be
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-26 09:36:14 +0330

    003 Implementing Signup Functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
-  
+
+  async signup(email:string, password:string){
+    // see if email is in use
+    const inUse = await this.usersService.find(email);
+    if(inUse){
+      throw new BadRequestException('email in use.')
+    }
+
+    // hash the user's password
+    
+    // create a new user and save it
+    // return user
+  }
+
+  signin(){
+
+  }
 }
