5c0c35f205ed38e01e7ea677b5fa6191da01f195
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-28 14:47:30 +0330

    011 Transforming Query String Data

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
@@ -15,6 +17,8 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApprovedReportDto } from './dtos/approved-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
+
 
 @Controller('reports')
 export class ReportsController {
@@ -27,11 +31,14 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
 
-
   @UseGuards(AdminGuard)
   @Patch('/:id')
-  approveReport(@Param('id') id: number,@Body() body:ApprovedReportDto){
-    return this.reportsService.changeApproval(id,body.approved )
+  approveReport(@Param('id') id: number, @Body() body: ApprovedReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query)
+  }
 }
