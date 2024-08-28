15ed50557d7e79220da68cf3cf1e93c4337559f1
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-28 15:41:57 +0330

    stop tracking .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,12 +47,18 @@ const cookieSession = require('cookie-session');
     },
   ],
 })
+
 export class AppModule {
+  constructor(
+    private configService:ConfigService
+  ){
+
+  }
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['heaoe'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
