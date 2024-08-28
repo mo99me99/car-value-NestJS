fb0c0fde18e120f2530fbd0459f8e99290a561fa
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-28 11:02:42 +0330

    009 The ManyToOne and OneToMany Decorators

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,20 +5,24 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
-  
+  OneToMany
 } from 'typeorm';
+import { Report } from 'src/reports/report.entity';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
-
+  
   @Column()
   email: string;
-
+  
   @Column()
   password: string;
 
+  @OneToMany(()=>Report, (report)=>report.user)
+  reports:Report[];
+  
   @AfterInsert()
   logInsert() {
     console.log('Inserted User With ID ', this.id);
